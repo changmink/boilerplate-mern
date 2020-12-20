aed42464a9ebdbef21a33408e8fd3f2433803fdc
changmink <changmin043@gmail.com>
2020-12-20 21:58:12 +0900

    [NoIssue] 로그인 페이지

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 function LandingPage() {
     return (
-        <div>
-            LandingPage            
+        <div style={{
+            display: 'flex', justifyContent: 'center', aliginItems: 'center', width: '100%', height: '100vh'
+        }}>
+            <h2>시작페이지</h2>            
         </div>
     )
 }

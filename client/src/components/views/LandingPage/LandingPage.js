6e43eab06316a98f8a6f7448d4d59789860be296
changmink <changmin043@gmail.com>
2020-12-20 22:28:56 +0900

    [NoIssue] 로그아웃 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,24 @@
 import React from 'react'
-
-function LandingPage() {
+import axios from 'axios'
+function LandingPage(props) {
+    const onClickHandler = () => {
+        axios.get('/api/users/logout')
+        .then(response => {
+            if(response.data.success) {
+                props.history.push('/login')
+            } else {
+                alert('logout fail')
+            }
+        })
+    }
     return (
         <div style={{
             display: 'flex', justifyContent: 'center', aliginItems: 'center', width: '100%', height: '100vh'
         }}>
-            <h2>시작페이지</h2>            
+            <h2>시작페이지</h2>
+            <button onClick={onClickHandler}>
+                로그 아웃
+            </button>         
         </div>
     )
 }
